a1ff9a5c6be6ee38a206d82c0c775956e3ae6a0c
Kuro3010 <saul.glez.alf@gmail.com>
2026-09-16 17:16:47 +0200

    Clase Game

--- src/tienda/Game.java
+++ src/tienda/Game.java
@@ -2,13 +2,66 @@ package tienda;
 
 public class Game {
 	
-	public int contador = 0;
-	public String nombre;
-	public double precio;
-	public Genre genero;
-	public String desarrollador;
-	public int id =+ contador;
+	
+	private int id;
+	private String titulo;
+	private double precio;
+	private Genre genero;
+	private int añadir;
+	private int stock;
+	
+	public Game(int id, String titulo, double precio, Genre genero, int stock) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.precio = precio;
+		this.genero = genero;
+		this.stock = stock;
+	}
+	
+	public int getId() {
+	    return id;
+	}
+
+	public String getTitulo() {
+	    return titulo;
+	}
+
+	public Genre getGenero() {
+	    return genero;
+	}
+
+	public double getPrecio() {
+	    return precio;
+	}
+
+	public int getStock() {
+	    return stock;
+	}
 	
 	
+	public void aumentarStock(int cantidad) {
+	    stock += cantidad;
+	}
+	
+	public void reducirStock(int cantidad) {
+	    if (cantidad <= stock) {
+	        stock -= cantidad;
+	    }
+	}
+	
+	public boolean hayStock() {
+	    return stock > 0;
+	}
+	
+	public String toString() {
+	    return	"Titulo:" + titulo + "\n" +
+	            "Precio:" + precio +  "\n" +
+	            "Genero=" + genero + "\n"+
+	            "Stock=" + stock +  "\n";
+	}
+
 	
 }
+
+
